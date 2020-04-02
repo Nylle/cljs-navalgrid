5bc5e63384d36401fc78ec037f55603cd125a991
Jan <Nylle@users.noreply.github.com>
2020-04-03 00:11:33 +0200

    Add convenience functions for adding squares to a map

--- src/navalgrid/mapbox.cljs
+++ src/navalgrid/mapbox.cljs
@@ -6,19 +6,34 @@
 (defn on-load [map f]
   (.on map "load" f))
 
+(defn remove-square [map id]
+  (if (.getLayer map id) (.removeLayer map id))
+  (if (.getSource map id) (.removeSource map id)))
+
+(defn fit-bounds [map coordinates]
+  (.fitBounds map (clj->js coordinates) (clj->js {:padding 200})))
+
 (defn add-square [map id width coordinates]
-  (.addLayer map
-             (clj->js
-              {:id     id,
-               :type   "line",
-               :source
+  (.addSource map
+              id
+              (clj->js
                {:type "geojson",
                 :data
                 {:type "Feature",
                  :geometry
-                 {:type "LineString", :coordinates coordinates}}},
+                 {:type "MultiLineString", :coordinates coordinates}}}))
+  (.addLayer map
+             (clj->js
+              {:id     id,
+               :type   "line",
+               :source id,
                :layout {:line-cap "square"},
                :paint  {:line-color "#038D3C", :line-width width}})))
 
-(defn fit-bounds [map coordinates]
-  (.fitBounds map (clj->js coordinates) (clj->js {:padding 200})))
+(defn set-square [map square]
+  (remove-square map "outer")
+  (remove-square map "inner")
+  (add-square map "outer" 3 (:OuterCoordinates square))
+  (add-square map "inner" 2 (:InnerCoordinates square))
+  (fit-bounds map (:Bounds square)))
+
