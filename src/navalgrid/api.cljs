4303991134b876914b9e04ddb4184bd59bddcc60
Jan <Nylle@users.noreply.github.com>
2020-04-06 08:45:03 +0200

    Make sure, rest-api calls are lower case
    Right now, the rest-api is just shoehorned into the normal web page
    which is configured for canonical urls. This causes a redirection to
    a lower-case-only url for api-requests as well. During such a redirect
    however the CORS-header gets lost resulting in an error in the browser
    (and a failed request). The recommended fix would be to exclude the
    api from canonical-url rules and allow requests as is. Until this is
    done however, the temporary fix of just lower-casing every request
    must suffice.

--- src/navalgrid/api.cljs
+++ src/navalgrid/api.cljs
@@ -2,4 +2,4 @@
   (:require [cljs-http.client :as http]))
 
 (defn get-square [id]
-  (http/get (str "http://localhost:54309/rest/square/" id) {:with-credentials? false}))
+  (http/get (str "http://localhost:54309/rest/square/" (clojure.string/lower-case id)) {:with-credentials? false}))
