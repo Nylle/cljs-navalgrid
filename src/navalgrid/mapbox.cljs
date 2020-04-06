02fc9635995c689d40efb1028c71d02a1d651115
Jan <Nylle@users.noreply.github.com>
2020-04-06 08:49:20 +0200

    Add marker with square label

--- src/navalgrid/mapbox.cljs
+++ src/navalgrid/mapbox.cljs
@@ -1,7 +1,13 @@
-(ns navalgrid.mapbox)
+(ns ^:figwheel-hooks navalgrid.mapbox
+  (:require [reagent.core :as r]))
+
+(defonce markers (r/atom []))
 
 (defn create-map [canvas & [center]]
   (js/createMap (reagent.dom/dom-node canvas) 0 (clj->js (or center [0 0]))))
+
+(defn create-marker [img]
+  (js/mapboxgl.Marker. img))
 
 (defn on-load [map f]
   (.on map "load" f))
@@ -11,7 +17,7 @@
   (if (.getSource map id) (.removeSource map id)))
 
 (defn fit-bounds [map coordinates]
-  (.fitBounds map (clj->js coordinates) (clj->js {:padding 200})))
+  (.fitBounds map (clj->js coordinates) (clj->js {:padding 150})))
 
 (defn add-square [map id width coordinates]
   (.addSource map
@@ -30,11 +36,33 @@
                :layout {:line-cap "square"},
                :paint  {:line-color "#038D3C", :line-width width}})))
 
+(defn add-marker [map coordinate img-src scale]
+  (let [img (js/Image.)]
+    (set! (.-onload img)
+      (fn []
+        (let [w (.-width img)
+              h (.-height img)]
+          (set! (.-width img) (* w scale))
+          (set! (.-height img) (* h scale)))))
+    (set! (.-src img) img-src)
+    (let [marker (create-marker img)]
+      (.setLngLat marker (clj->js coordinate))
+      (.addTo marker map)
+      (swap! markers conj marker))))
+
+(defn clear-markers []
+  (doseq [marker @markers]
+    (.remove marker))
+  (reset! markers []))
+
 (defn set-square [map square]
   (remove-square map "outer")
   (remove-square map "inner")
   (add-square map "outer" 3 (:OuterCoordinates square))
   (add-square map "inner" 2 (:InnerCoordinates square))
+  (clear-markers)
+  (add-marker map (:Center square) (:LabelUrl square) 0.75)
+  ;;TODO: sub labels!
   (fit-bounds map (:Bounds square)))
 
 (defn set-all-squares [map square]
