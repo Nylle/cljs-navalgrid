b27b357177ba7a4a86bcd107256370eb3b2f1429
Jan <Nylle@users.noreply.github.com>
2020-04-13 18:00:11 +0200

    Change polygon creation to work with new API
    The new API does not return coordinate objects and lists but simple
    double[] representing lnglats.

--- tests/navalgrid/geo-tests.cljs
+++ tests/navalgrid/geo-tests.cljs
@@ -2,21 +2,18 @@
   (:require [navalgrid.geo :as sut]
             [cljs.test :refer-macros [deftest is testing run-tests]]))
 
-(deftest test-lnglat
-  (testing "A coordinate object is converted to GeoJSON"
-           (is (= [0 9] (sut/lnglat {:Latitude 9 :Longitude 0})))))
-
-(deftest test-polygon
+(deftest test--polygon
   (testing "For a GeoJSON polygon"
            (is
             (= [[1 1] [2 2] [3 3] [1 1]]
-               (sut/polygon
-                [{:Latitude 1 :Longitude 1}
-                 {:Latitude 2 :Longitude 2}
-                 {:Latitude 3 :Longitude 3}]))
-            "coordinates are mapped to lnglat vectors and the first coordinate is appended to 'close' the polygon")))
+               (sut/polygon [[1 1] [2 2] [3 3]]))
+            "the first coordinate is appended to 'close' the polygon")
+           (is
+            (= [[-160 90] [-190 90] [-160 90]]
+               (sut/polygon [[-160 90] [170 90]]))
+            "the fix for crossing the anti-meridian in mapbox is applied")))
 
-(deftest test-fix-for-antimeridian
+(deftest test--fix-for-antimeridian
   (testing "Coordinates on either side of the antimeridian are 'fixed' for mapbox"
            (is
             (= [[-160 90] [-190 90]]
@@ -33,5 +30,3 @@
                (sut/fix-for-antimeridian
                 [[160 90] [-170 90] [160 90] [170 90]]))
             "with multiple positive values")))
-
-(cljs.test/run-tests)
